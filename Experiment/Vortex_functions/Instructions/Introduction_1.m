c5fcf20f16cb4271be77c3699a076f12681cb644
Fabrice Luyckx <FabriceLuyckx@users.noreply.github.com>
2017-01-25 18:34:12 +0000

    Add files via upload

---
+++ Experiment/Vortex_functions/Instructions/Introduction_1.m
@@ -0,0 +1,10 @@
+In the following experiment it will be your task to catch coins,
+flying from the middle of the screen.
+
+You will move your bucket around on the big outer circle, 
+using the turning wheel. Before each new coin, you can choose 
+a new location for your bucket. Basically, you need to anticipate 
+where the next coin will land.
+
+Usually the coins will fly at approximately the same location. But sometimes
+this location will change!
---
+++ Experiment/Vortex_functions/Instructions/Introduction_2.m
@@ -0,0 +1,13 @@
+After you have positioned the bucket, you can indicate your 
+confidence in how likely you believe the coin is going to 
+land in your bucket.
+
+If the coin hits your bucket, you win 10 points.  But if it's out, 
+you lose 10 points.
+
+When you have positioned your bucket, press SPACEBAR to indicate 
+your confidence. Use the touchpad to change the number. After you 
+have chosen a number, press SPACEBAR again to shoot the coin.
+Try to respond as quickly and accurately as possible. 
+
+First you will do one practice block of 20 coins to get used to the task.
