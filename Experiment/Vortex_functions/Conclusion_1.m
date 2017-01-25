7f55f5cc08529058ff0b9fb609718e9bc7bdd03a
Fabrice Luyckx <FabriceLuyckx@users.noreply.github.com>
2017-01-25 18:33:42 +0000

    Add files via upload

---
+++ Experiment/Vortex_functions/Conclusion_1.m
@@ -0,0 +1,13 @@
+Now the real experiment will start.
+
+You will go through 4 blocks of 75 coins. You can take a break in between. 
+
+Remember:
+    1) Choose a position for your bucket and confirm with SPACE
+    2) Indicate your confidence, confirm again with SPACE
+    3) If you catch the coin, you win 10 points, otherwise you
+        lose 10 points.
+
+The more points you gather, the more money you earn.
+
+Please call the experimenter if you have any questions. Good luck!
